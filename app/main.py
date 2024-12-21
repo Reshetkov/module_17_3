a6718058c26a84208f2a6242f4ba7b9499e8386c
Reshetkov <alexander.reshetkov.cb@gmail.com>
2024-12-21 09:55:11 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from routers import task
+from routers import user
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome():
+    return {"message": "Welcome to Taskmanager"}
+
+
+app.include_router(user.router)
+
+app.include_router(task.router)
+
+#  python -m uvicorn main:app
+# cd app
+# alembic init app/migrations
+# alembic revision --autogenerate -m "Initial migration"
+# alembic upgrade head
